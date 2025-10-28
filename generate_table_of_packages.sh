4b20987f5c87f109b183d2eed619a21e6a32c2a2
Yifei Sun <ysun@hey.com>
2025-10-28 10:47:48 +0100

    treewide: get rid of old files

--- generate_table_of_packages.sh
+++
@@ -1,2 +0,0 @@
-OUTPUT=packages.md
-nix flake show --json | jq -j --raw-output '["|attr|package|description|\n","|:-----|:------|:------|\n"] + (.packages."x86_64-linux" | to_entries | map("|\(.key)|\(.value.name)|\(.value.description | gsub("\n"; " "))|\n")) | .[]' > ${OUTPUT}
